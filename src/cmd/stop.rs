cd6f71fedae90230e07fcbd85bbaeaafd12b14f7
Duc Tho Tran <ducthotran2010@gmail.com>
2021-07-09 11:43:04 +0700

    docker kill node before down

--- src/cmd/stop.rs
+++ src/cmd/stop.rs
@@ -5,6 +5,7 @@ pub fn cmd<'a>() -> Command<'a, ()> {
     Command::new("stop")
         .description("Stop all services")
         .runner(|_env, _matches| {
+            duct::cmd("docker", vec!["kill", "node"]).run().unwrap();
             duct::cmd("docker-compose", vec!["down"]).run().unwrap();
             Ok(())
         })
